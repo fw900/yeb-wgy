7f67c6682a51064879627065d16e1eb6fa33c013
wgy <wgy@qq.com>
2021-09-27 15:46:48 +0800

    MyBatisPlusConfig分页配置

---
+++ yeb-server/src/main/java/com/fw/server/config/MyBatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.fw.server.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlus分页配置
+ *
+ * @author FW900
+ * @date 2021-09-26 11:13
+ */
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
